6fb7557d7f45920fd7d75d0ee384fec37ede5041
Irina Belyaeva <ibelyaev@jcvi.org>
2016-01-06 13:49:43 -0500

    fixed exp service

--- services/experiments_api/experiments_service.py
+++ services/experiments_api/experiments_service.py
@@ -15,18 +15,24 @@ log.setLevel(logging.INFO)
 def get_experiment_by_id(url, args):
 
     lookup_id = args['experimentID']
+
+    try:
+        int_lookup_id = int(lookup_id)
+    except ValueError:
+        raise Exception("Non integer experiment ID was submitted!")
+
     log.debug("Experiment Lookup Id:" + str(lookup_id))
     response = get_experiment_as_objects(url, args)
 
     if not response:
         raise Exception ("Error ocurred. Cannot load experiments to search for experiment ID.")
 
-    lookup_object = find(lambda item: item.expId == 106, response)
+    lookup_object = find(lambda item: item.expId == int_lookup_id, response)
     log.debug(lookup_object)
 
     # raise not found exception if no such experiment
     if not lookup_object:
-        raise exception.NotFound("No experiment found for experiment ID" + str(lookup_id))
+        raise exception.NotFound("No experiment found for experiment ID: " + str(lookup_id))
 
     lookup_object_as_json_string = jsonpickle.encode(lookup_object, unpicklable=False)
     log.debug("JSON deserialization:")
